3e2874b2f5bfc67fee2c4e867d03823ac0553676
LazyPanda07 <semengricenko@gmail.com>
2026-02-09 18:30:36 +0500

    Update version

--- src/DatabaseUtility.cpp
+++ src/DatabaseUtility.cpp
@@ -4,7 +4,7 @@ namespace database::utility
 {
 	constexpr std::string_view getVersion()
 	{
-		constexpr std::string_view version = "1.4.0";
+		constexpr std::string_view version = "1.4.1";
 
 		return version;
 	}
